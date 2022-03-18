8df498cacefc6379ef531a9796707ce4a43ba3ec
Mukesh Bishnoi <mukeshbishnoi@gmail.com>
2022-03-19 00:44:07 +0530

    feat: added cart and wishlist page

---
+++ src/pages/CartPage/CartPage.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './CartPage.css';
+import {Nav} from './../../components/Nav/Nav';
+import { VerticalCard } from '../../components/Card/VerticalCard';
+import {useCartContext} from './../../Context/CardContext';
+import {PriceCard} from './../../components/Card/PriceCard';
+
+const CartPage = () => {
+    const {cartState:{cart}} = useCartContext();
+    // console.log(cart);
+    
+    return (
+        <>
+            <Nav />
+            <div className="cart-container">
+                <div className="Cart-heading">
+                    <h1 className="cart-heading">Cart Items</h1>
+                    <div className="cart-items">
+                    <div className="item-container">
+                        {cart.map(item => <VerticalCard data={item} key={item.id}/>)} 																
+                    </div>
+                    <div className="checkout-container">
+                        <PriceCard />																			
+                    </div>
+                </div>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export { CartPage }
